0cb612534bea95ea3682716ff52fe5b732a9cdce
akhilrajs <67222042+akhilrajs@users.noreply.github.com>
2022-08-11 18:44:59 +0530

    Update XPATH.py

--- XPATH.py
+++ XPATH.py
@@ -1,2 +1,2 @@
-menu_btn = "/html/body/div[1]/div/div/div[3]/header/div[2]/div/span/div[3]/div/span"
 click_btn = "/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[2]/div[2]/button/span"
+menu_btn = "/html/body/div[1]/div/div/div[3]/header/div[2]/div/span/div[3]/div/span"
